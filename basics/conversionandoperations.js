6e87fef4ccdc6a4d9eb5797e4766786c7bb5f6b1
Deepika vk <106546916+deepika-vk@users.noreply.github.com>
2024-03-18 08:38:33 +0000

    conversions

---
+++ basics/conversionandoperations.js
@@ -0,0 +1,14 @@
+let score="33abc"
+let a=null
+let b=true
+console.log(score);
+console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+let v=Number(b);
+console.log(v);
+
+let isloggedin=1
+let l =Boolean(isloggedin);
+console.log(l);
