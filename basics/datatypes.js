c104c549e1a52882580d30bbc8bc187bc5e31520
Deepika vk <106546916+deepika-vk@users.noreply.github.com>
2024-03-18 07:59:16 +0000

    datatype and variable

---
+++ basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"
+//alert(3+3)// we are using Node.js not browser
+console.log(3+3) //code readability shd be high
+console.log("deepika");
+
+let name="Deepika"
+let age=20
+let isloggedIn=false
+
+//number=>2 to power 53
+//bigint 
+//string =>""
+//boolean =>true/false
+//null=>standalone value
+//undefined =>
+//symbol =>unique
+
+//object
+
+console.log(typeof Symbol);
---
+++ basics/variables.js
@@ -0,0 +1,8 @@
+const a=12345
+let b="Deepika";
+var c="1234"
+city="karnataka"
+let C;
+console.log(a);
+
+console.table([a,b,c,city,C]);
