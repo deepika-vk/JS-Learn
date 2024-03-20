bacf7d39504a74fbe59ae4af5e4680d7b60d6471
Deepika vk <106546916+deepika-vk@users.noreply.github.com>
2024-03-20 14:06:54 +0000

    stack&memory

---
+++ basics/memory.js
@@ -0,0 +1,14 @@
+//stack(primitive) heap (non premitive)
+
+let myyotube ="do more be more"
+let anothername=myyotube;
+anothername="code and fun";
+console.log(anothername);
+let userone={
+    email:"user@google.com",
+    upi:"user@ybl"
+}
+let usertwo=userone;
+usertwo.email="deepika@123.com"
+console.log(userone.email);
+console.log(usertwo.email);
